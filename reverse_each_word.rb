92e34f570d164060761590fd05a64eeedb9c92ef
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-16 14:51:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
 def reverse_each_word(string)
    
-   #string.split(" ").collect {|word| word.reverse}.join(" ")
-   
-   new_array =[]
-  array = string.split()
-  array.each do |words|
-   new_array << words.reverse
-  end
-  return new_array.join(" ")
+  new_array = []
   
+  array = string.split
+  
+  array.each {|words| new_array << words.reverse}
+  
+end 
+
+  return new_array.join(" ")
 
 end
 
